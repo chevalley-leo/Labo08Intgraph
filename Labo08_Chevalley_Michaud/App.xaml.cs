de24c91b7e3e6e296d4a67d969b8fa8521dffd2e
Léo Chevalley <leo.chevalley@heig-vd.ch>
2024-12-17 16:33:30 +0100

    sfvrthzthrd

---
+++ Labo08_Chevalley_Michaud/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Labo08_Chevalley_Michaud
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
